6307444ea8816916d0693b7f2791c75a79c18caa
Daarien <35134108+Daarien@users.noreply.github.com>
2020-01-23 14:17:33 +0300

    fix: Button component props

--- src/components/custom/Buttons/SimpleButton.tsx
+++ src/components/custom/Buttons/SimpleButton.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import classnames from 'classnames';
-import { IButton } from './index';
+import React from "react";
+import classnames from "classnames";
+import { IButton } from "./index";
 
 export function SimpleButton({ className, variant, ...other }: IButton) {
-	return <button type='button' className={classnames('btn', `btn-${variant}`, { [className!]: className })} {...other} />;
+  return (
+    <button
+      type="button"
+      className={classnames("btn", `btn-${variant}`, {
+        [className!]: className
+      })}
+      {...other}
+    />
+  );
 }
 
 SimpleButton.defaultProps = {
-	variant: 'primary',
-	active: false,
-	disabled: false,
-	type: 'button'
+  variant: "primary",
+  active: false,
+  disabled: false
 };
--- src/components/custom/Buttons/index.ts
+++ src/components/custom/Buttons/index.ts
@@ -1,15 +1,14 @@
-export { SimpleButton } from './SimpleButton';
+export { SimpleButton } from "./SimpleButton";
 
 export interface IButton {
-	className: string;
-	type: string;
-	variant?:
-	| "primary"
-	| "secondary"
-	| "info"
-	| "search"
-	| "outline-primary"
-	| "outline-secondary";
-	onClick?: React.MouseEventHandler<HTMLButtonElement>;
-	children?: React.ReactNode;
+  className: string;
+  variant?:
+    | "primary"
+    | "secondary"
+    | "info"
+    | "search"
+    | "outline-primary"
+    | "outline-secondary";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children?: React.ReactNode;
 }
